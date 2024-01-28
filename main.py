52452906682ddacb2f305d5ee2331c85ca02733b
Benjamin Buckmaster <benjaminbuckmaster@icloud.com>
2024-01-29 10:58:45 +1100

    Created graphing capadibilties

---
+++ main.py
@@ -0,0 +1,69 @@
+import dash
+from dash import html, dash_table, dcc
+import sqlite3
+import pandas as pd
+
+# Connect to SQLite database
+conn = sqlite3.connect('aquarium.db')
+cursor = conn.cursor()
+
+# Create a Dash web application
+app = dash.Dash(__name__)
+
+# SQL query
+query = "select date, pH, ammonia, nitrite, nitrate, KH, GH from water_param"
+
+# Fetch the results into a Pandas DataFrame
+df = pd.read_sql_query(query, conn)
+
+mode = "lines+markers"
+
+# Define your layout
+app.layout = html.Div([
+    # html.H1("Aquarium Water Parameter Visualiser v0.1"),
+    # Checklist for selecting data series to display
+    dcc.Checklist(
+        id='series-selector',
+        options=[
+            {'label': 'pH', 'value': 'pH'},
+            {'label': 'Ammonia', 'value': 'ammonia'},
+            {'label': 'Nitrite', 'value': 'nitrite'},
+            {'label': 'Nitrate', 'value': 'nitrate'},
+            {'label': 'KH', 'value': 'KH'},
+            {'label': 'GH', 'value': 'GH'}
+        ],
+        value=['pH', 'ammonia', 'nitrite', 'nitrate', 'KH', 'GH'],
+        inline=True
+    ),
+    
+    dcc.Graph(
+        id='line-chart',
+        figure={}
+    )
+])
+
+# Callback to update the graph based on selected series
+@app.callback(
+    dash.dependencies.Output('line-chart', 'figure'),
+    [dash.dependencies.Input('series-selector', 'value')]
+)
+def update_graph(selected_series):
+    data = []
+
+    # Create a trace for each selected series
+    for series in selected_series:
+        data.append({'x': df['date'], 'y': df[series], 'mode': mode, 'name': series})
+
+    return {
+        'data': data,
+        'layout': {
+            'title': 'Water Quality over Time',
+            'xaxis': {'title': 'Date'},
+            'yaxis': {'title': 'Values'},
+            'hovermode': 'closest'
+        }
+    }
+
+# Run the app
+if __name__ == '__main__':
+    app.run_server(debug=True)
